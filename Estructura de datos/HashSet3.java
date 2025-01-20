0cc3dca83995d66ab4b5123cbadf9c540f5c1ad2
IvanRubert <114068487+IvanRubert@users.noreply.github.com>
2025-01-20 18:43:07 +0000

    Correccion HashSet3

--- Estructura de datos/HashSet3.java
+++ Estructura de datos/HashSet3.java
@@ -4,20 +4,26 @@ import java.util.Set;
 
 public class HashSet3 {
     public static void main(String[] args) {
-        Set<String> a = new HashSet<>();
-        Set<String> b = new HashSet<>();
-        a.addAll(Arrays.asList(new String[] {"Hola", "prueba", "esta", "duplicado"}));
-        b.addAll(Arrays.asList(new String[] {"de", "esta", "esto", "es", "Hola"}));
+        String[] array = {"Hola", "prueba", "esta", "duplicado", "de", "esta", "esto", "es", "Hola"};
 
         Set<String> diferencia = new HashSet<>();
-        diferencia = mantenerDiferenciaListas(a, b);
+        diferencia = mantenerDiferenciaListas(array);
 
         System.out.println(diferencia);
     }
 
-    private static Set<String> mantenerDiferenciaListas(Set<String> a, Set<String> b) {
-        Set<String> diferencia = new HashSet<>(a);
-        diferencia.removeAll(b);
-        return diferencia;
+    private static Set<String> mantenerDiferenciaListas(String[] array) {
+        Set<String> a = new HashSet<>();
+        Set<String> repetido = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
+            if (a.contains(array[i])){
+                repetido.add(array[i]);
+            }else{
+                a.add(array[i]);
+            }
+        }
+        return repetido;
     }
 }
+
+
